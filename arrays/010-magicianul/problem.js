a3580b87941012e1c617c8bd465dac30f5751f7e
sorinvestemean <vestemeansorin@gmail.com>
2022-05-12 21:56:57 +0300

    added magicianul exercise

---
+++ arrays/010-magicianul/problem.js
@@ -0,0 +1,12 @@
+/*
+    Esti la un spectacol de magie, magicianul te cheama pe scena sa tragi o carte de joc.
+    Ce carte de joc ai tras?
+
+    INPUT
+        pickAnyCard();
+    OUTPUT
+        7 de trefla
+        10 de inima rosie
+        J de inima neagra
+        Q de romb
+*/
