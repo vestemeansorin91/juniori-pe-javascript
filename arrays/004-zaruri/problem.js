6e95279a1ac788b615d883b9a1f8ef1ac7d01a0e
sorinvestemean <vestemeansorin@gmail.com>
2022-05-13 21:54:32 +0300

    zaruri added

--- arrays/004-zaruri/problem.js
+++ arrays/004-zaruri/problem.js
@@ -3,19 +3,22 @@
     Aruncati pe rand cu ele si vreti sa vedeti ce zaruri a dat fiecare si cine a castigat. (castiga cel cu totalul mai mare).
     Functia nu primeste niciun parametru si afisati in consola.
 
-    INPUT
-        drawDices();
-
-    OUTPUT
-        Jucatorul 1 a dat 4-2
-        Jucatorul 2 a dat 3-6
-        Jucatorul 2 a castigat!
+    const player1 = 'Sorin';
+    const player2 = 'Alex';
 
     INPUT
-        drawDices();
+        drawDices(player1, player2);
 
     OUTPUT
-        Jucatorul 1 a dat 3-3
-        Jucatorul 2 a dat 1-5
+        Sorin 1 a dat 4-2
+        Alex 2 a dat 3-6
+        Alex 2 a castigat!
+
+    INPUT
+        drawDices(player1, player2);
+
+    OUTPUT
+        Sorin 1 a dat 3-3
+        Alex 2 a dat 1-5
         Remiza!
 */
